f836e0d780d7e961933fff5ca3a9d6d1fbac079d
bhardwajj03 <adi.bhardwaj09@gmail.com>
2024-06-14 18:56:03 +0530

    auth changes

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen
+          name='Sign-In'
+          options={{
+            headerShown:false
+          }} 
+        />
+        <Stack.Screen
+          name='Sign-Up'
+          options={{
+            headerShown:false
+          }} 
+        />
+      </Stack>
+      <StatusBar backgroundColor='#161622' style='light'/>
+    </>
   )
 }
 
 export default AuthLayout
 
-const styles = StyleSheet.create({})
